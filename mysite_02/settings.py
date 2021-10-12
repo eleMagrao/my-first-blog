a808e8b969db8893a4877de2c5570208914e3d2f
magrao <magrao@gmail.com>
2021-10-12 18:18:04 -0300

    Aaaa

--- mysite_02/settings.py
+++ mysite_02/settings.py
@@ -78,7 +78,8 @@ WSGI_APPLICATION = 'mysite_02.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
